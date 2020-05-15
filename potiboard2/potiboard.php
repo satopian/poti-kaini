8ac5810b2c42ec90f77a02cb14738b420c28b0c6
sakots <sakots8000@gmail.com>
2020-05-15 14:09:02 +0900

    v2.0.5

--- potiboard2/potiboard.php
+++ potiboard2/potiboard.php
@@ -3,7 +3,7 @@
 //$time_start = microtime(true);
 /*
   *
-  * POTI-board改二 v2.0.4 lot.200515
+  * POTI-board改二 v2.0.5 lot.200515
   *   (C)sakots >> https://poti-k.info/
   *
   *----------------------------------------------------------------------------------
@@ -42,8 +42,8 @@
 */
 
 //バージョン
-define('POTI_VER' , 'v2.0.4');
-define('POTI_VERLOT' , 'v2.0.4 lot.200515');
+define('POTI_VER' , 'v2.0.5');
+define('POTI_VERLOT' , 'v2.0.5 lot.200515');
 
 if(phpversion()>="5.5.0"){
 //スパム無効化関数
@@ -1043,7 +1043,8 @@ function regist($name,$email,$sub,$com,$url,$pwd,$upfile,$upfile_name,$resto,$pi
 		$pxck = "on";
 	}
 
-	if($pxck=="on" && PROXY_CHECK){
+	//if($pxck=="on" && PROXY_CHECK){
+	if($pxck=="on"){
 		if(proxy_connect('80') == 1){
 			error(MSG017,$dest);
 		}elseif(proxy_connect('8080') == 1){
@@ -1097,9 +1098,9 @@ function regist($name,$email,$sub,$com,$url,$pwd,$upfile,$upfile_name,$resto,$pi
 	$com = str_replace("\r", "\n", $com);
 	// 連続する空行を一行
 	$com = preg_replace("/\n((　| )*\n){3,}/","\n",$com);
-	if(!BR_CHECK || substr_count($com,"\n")<BR_CHECK){
-		$com = nl2br($com);		//改行文字の前に<br>を代入する
-	}
+	//if(!BR_CHECK || substr_count($com,"\n")<BR_CHECK){
+	//	$com = nl2br($com);		//改行文字の前に<br>を代入する
+	//}
 	$com = str_replace("\n", "", $com);	//\nを文字列から消す
 
 	$name=preg_replace("/◆/","◇",$name);
@@ -2401,7 +2402,8 @@ function rewrite($no,$name,$email,$sub,$com,$url,$pwd,$admin){
 		$pxck = "on";
 	}
 
-	if($pxck=="on" && PROXY_CHECK){
+	//if($pxck=="on" && PROXY_CHECK){
+	if($pxck=="on"){
 		if(proxy_connect('80') == 1){
 			error(MSG017);
 		}elseif(proxy_connect('8080') == 1){
