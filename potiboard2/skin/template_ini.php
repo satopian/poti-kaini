f5f50a9dd2a20c35f01e78451ab512fa7a89c402
sakots <sakots8000@gmail.com>
2020-05-16 23:09:46 +0900

    スキン更新

--- potiboard2/skin/template_ini.php
+++ potiboard2/skin/template_ini.php
@@ -1,13 +1,13 @@
 <?php
 /*
-  * Template - MONO - v1.1.0 lot.200516a  by sakots  >> https://poti-k.info/
+  * Template - MONO by sakots  >> https://poti-k.info/
   *
   * potiboard.php(v2.2.0～)のTemplate設定ファイルです。
   *
 */
 
 //テンプレートのバージョン
-define('TEMPLATE_VER', "v1.1.0 lot.200516a");
+define('TEMPLATE_VER', "v1.1.1 lot.200516b");
 
 //エラーメッセージ
 define('MSG001', "該当記事がみつかりません[Log is not found.]");
