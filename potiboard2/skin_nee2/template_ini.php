5803f8de3563b7d9975de8de171580aa01005d3a
sakots <sakots8000@gmail.com>
2020-05-16 15:05:46 +0900

    スキンnee2更新

--- potiboard2/skin_nee2/template_ini.php
+++ potiboard2/skin_nee2/template_ini.php
@@ -1,13 +1,13 @@
 <?php
 /*
-  * Template - nee2 - v1.1.0 lot.200516  by sakots  >> https://poti-k.info/
+  * Template - nee2 - v1.1.1 lot.200516a  by sakots  >> https://poti-k.info/
   *
   * potiboard.php(v2.2.0～)のTemplate設定ファイルです。
   *
 */
 
 //テンプレートのバージョン
-define('TEMPLATE_VER', "v1.1.0 lot.200516");
+define('TEMPLATE_VER', "v1.1.1 lot.200516a");
 
 //エラーメッセージ
 define('MSG001', "該当記事がみつかりません[Log is not found.]");
