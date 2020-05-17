347a0acdeb05a5e35b51b1d371b8485aff093e34
sakots <sakots8000@gmail.com>
2020-05-17 17:23:52 +0900

    スキン更新

--- potiboard2/skin/template_ini.php
+++ potiboard2/skin/template_ini.php
@@ -7,7 +7,7 @@
 */
 
 //テンプレートのバージョン
-define('TEMPLATE_VER', "v1.3.0 lot.200517c");
+define('TEMPLATE_VER', "v1.3.1 lot.200517d");
 
 //エラーメッセージ
 define('MSG001', "該当記事がみつかりません[Log is not found.]");
