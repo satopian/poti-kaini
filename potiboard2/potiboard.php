df869a2036a3bb4757d828bbceea008b3f9163f7
sakots <sakots8000@gmail.com>
2020-05-18 22:13:14 +0900

    v2.3.4

--- potiboard2/potiboard.php
+++ potiboard2/potiboard.php
@@ -42,8 +42,8 @@
 */
 
 //バージョン
-define('POTI_VER' , 'v2.3.3');
-define('POTI_VERLOT' , 'v2.3.3 lot.200518a');
+define('POTI_VER' , 'v2.3.4');
+define('POTI_VERLOT' , 'v2.3.4 lot.200518b');
 
 if(phpversion()>="5.5.0"){
 //スパム無効化関数
