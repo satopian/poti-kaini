61183e4840a0d230cbe90251372041ee3acd1301
sakots <sakots8000@gmail.com>
2020-05-19 00:48:57 +0900

    v2.4.0

--- potiboard2/potiboard.php
+++ potiboard2/potiboard.php
@@ -42,8 +42,8 @@
 */
 
 //バージョン
-define('POTI_VER' , 'v2.3.4');
-define('POTI_VERLOT' , 'v2.3.4 lot.200518b');
+define('POTI_VER' , 'v2.4.0');
+define('POTI_VERLOT' , 'v2.4.0 lot.200519');
 
 if(phpversion()>="5.5.0"){
 //スパム無効化関数
--- potiboard2/skin_nee2/template_ini.php
+++ potiboard2/skin_nee2/template_ini.php
@@ -7,7 +7,7 @@
 */
 
 //テンプレートのバージョン
-define('TEMPLATE_VER', "v1.2.10 lot.200518d1e");
+define('TEMPLATE_VER', "v1.3.0 lot.200519");
 
 //エラーメッセージ
 define('MSG001', "該当記事がみつかりません[Log is not found.]");
