faa263a09de8d9b00c0f8c7abd59bf367c3dcafd
sakots <sakots8000@gmail.com>
2020-08-06 22:53:39 +0900

    テーマ

--- potiboard2/theme/template_ini.php
+++ potiboard2/theme/template_ini.php
@@ -7,7 +7,7 @@
 */
 
 //テーマ(テンプレート)のバージョン
-define('TEMPLATE_VER', "v1.9.0 lot.200803");
+define('TEMPLATE_VER', "v1.10.0 lot.200806");
 
 //エラーメッセージ
 define('MSG001', "該当記事がみつかりません[Log is not found.]");
