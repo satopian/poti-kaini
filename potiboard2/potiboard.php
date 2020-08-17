e1134e99ac9177d79eb335c838b48a44b502d3e1
sakots <sakots8000@gmail.com>
2020-08-18 02:12:44 +0900

    v2.12.5

--- potiboard2/potiboard.php
+++ potiboard2/potiboard.php
@@ -42,8 +42,8 @@ define('USE_DUMP_FOR_DEBUG','0');
 */
 
 //バージョン
-define('POTI_VER' , 'v2.12.4');
-define('POTI_VERLOT' , 'v2.12.4 lot.200817');
+define('POTI_VER' , 'v2.12.5');
+define('POTI_VERLOT' , 'v2.12.5 lot.200818');
 
 if (($phpver = phpversion()) < "5.5.0") {
 	die("本プログラムの動作には PHPバージョン 5.5.0 以上が必要です。<br>\n（現在のPHPバージョン：{$phpver}）");
