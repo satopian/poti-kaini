579d9fc6cdadab13760aa5c2df963a463534f7a5
sakots <sakots8000@gmail.com>
2021-02-15 21:01:20 +0900

    v2.26.1

--- potiboard2/potiboard.php
+++ potiboard2/potiboard.php
@@ -6,7 +6,7 @@ define('USE_DUMP_FOR_DEBUG','0');
 // POTI-board改二 
 // バージョン :
 define('POTI_VER','v2.26.1');
-define('POTI_LOT','lot.210214.0'); 
+define('POTI_LOT','lot.210215.0'); 
 
 /*
   (C)sakots >> https://poti-k.info/
