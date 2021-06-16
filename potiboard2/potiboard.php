57a8e963b8aace552d3da476ebdbbaa6eb604617
satopian <satopian@mia.eek.jp>
2021-06-16 16:49:07 +0900

    if(!(get_gd_ver() && (ImageTypes() & IMG_JPG))){ ネストの整理

--- potiboard2/potiboard.php
+++ potiboard2/potiboard.php
@@ -263,16 +263,14 @@ function gd_check(){
 	$check = array("ImageCreate","ImageCopyResized","ImageCreateFromJPEG","ImageJPEG","ImageDestroy");
 
 	//最低限のGD関数が使えるかチェック
-	if(get_gd_ver() && (ImageTypes() & IMG_JPG)){
-		foreach ( $check as $cmd ) {
-			if(!function_exists($cmd)){
-				return false;
-			}
-		}
-	}else{
+	if(!(get_gd_ver() && (ImageTypes() & IMG_JPG))){
 		return false;
 	}
-
+	foreach ( $check as $cmd ) {
+		if(!function_exists($cmd)){
+			return false;
+		}
+	}
 	return true;
 }
 
