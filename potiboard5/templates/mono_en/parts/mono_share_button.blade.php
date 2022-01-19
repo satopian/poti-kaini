b51b7cb52862fb280027a2f2736d13c895253edb
satopian <satopian@mia.eek.jp>
2022-01-19 16:59:44 +0900

    en

---
+++ potiboard5/templates/mono_en/parts/mono_share_button.blade.php
@@ -0,0 +1,4 @@
+<span class="share_button">
+	<a target="_blank" href="https://twitter.com/intent/tweet?&text=%5B{{$ress[0]['encoded_no']}}%5D%20{{$ress[0]['share_sub']}}%20by%20{{$ress[0]['share_name']}}%20-%20{{$encoded_title}}&url={{$encoded_rooturl}}{{$encoded_self}}?res={{$ress[0]['encoded_no']}}"><span class="icon-twitter button"><img src="{{$skindir}}img/twitter.svg" alt=""> tweet</span></a>
+	<a target="_blank" class="fb btn" href="http://www.facebook.com/share.php?u={{$encoded_rooturl}}{{$encoded_self}}?res={{$ress[0]['encoded_no']}}"><span class="icon-facebook2 button"><img src="{{$skindir}}img/facebook.svg" alt=""> share</span></a>
+</span>
