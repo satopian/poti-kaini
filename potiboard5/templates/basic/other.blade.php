0c19d4bbcdbe3f63142c88b095e459b9ee2c8ebf
satopian <satopian@mia.eek.jp>
2022-01-31 23:14:31 +0900

    @if($post_mode and !$rewrite) 投稿フォーム @endif

--- potiboard5/templates/basic/other.blade.php
+++ potiboard5/templates/basic/other.blade.php
@@ -12,8 +12,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 	<link rel="stylesheet" href="{{$skindir}}basic.css">
-	<title>@if($post_mode) @if(!$rewrite) @if(!$admin_del) @if(!$admin) 投稿フォーム @endif @endif @endif @endif @if($rewrite)
-		編集モード @endif @if($admin_in) 管理用 @endif @if($admin) @if($rewrite) - @endif @endif @if($admin) 管理人による投稿 @endif
+	<title>@if($post_mode and !$rewrite) 投稿フォーム @endif @if($rewrite)
+		編集モード @endif @if($admin_in) 管理用 @endif @if($admin) 管理人による投稿 @endif
 		@if($admin_del) 記事削除 @endif @if($err_mode) エラー！ @endif - {{$title}} </title>
 	{{-- <!-- 
 // title…掲示板タイトル
@@ -34,8 +34,8 @@
 <body>
 	<div id="body">
 		<header>
-			<h1 id="bbs_title">@if(!$rewrite) @if(!$admin_del) @if(!$admin) 投稿フォーム @endif @endif @endif @if($rewrite)
-				編集モード @endif @if($admin_in) 管理用 @endif @if($admin) @if($rewrite) - @endif @endif @if($admin) 管理人による投稿
+			<h1 id="bbs_title">@if($post_mode and !$rewrite )投稿フォーム @endif @if($rewrite)
+				編集モード @endif @if($admin_in) 管理用 @endif @if($admin) 管理人による投稿
 				@endif @if($admin_del) 記事削除 @endif @if($err_mode) エラー！ @endif - <span
 					class="title_name_wrap">{{$title}}</span></h1>
 			{{-- <!-- 
