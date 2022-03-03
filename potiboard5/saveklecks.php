e90a144ced65a6fb0fda6fd76123a7d63678cecc
satopian <satopian@mia.eek.jp>
2022-03-03 21:45:15 +0900

    if(!SIZE_CHECK || ($_FILES['picture']['size'] < (PSD_MAX_KB * 1024))){ //psd容量チェック

--- potiboard5/saveklecks.php
+++ potiboard5/saveklecks.php
@@ -7,6 +7,7 @@ include(__DIR__.'/config.php');
 define('SIZE_CHECK', '1');
 //PNG画像データ投稿容量制限KB(chiは含まない)
 define('PICTURE_MAX_KB', '5120');//5MBまで
+define('PSD_MAX_KB', '10240');//10MBまで。ただしサーバのPHPの設定によって2MB以下に制限される可能性があります。
 defined('PERMISSION_FOR_LOG') or define('PERMISSION_FOR_LOG', 0600); //config.phpで未定義なら0600
 defined('PERMISSION_FOR_DEST') or define('PERMISSION_FOR_DEST', 0606); //config.phpで未定義なら0606
 defined('CRYPT_METHOD') or define('CRYPT_METHOD','aes-128-cbc');
@@ -59,8 +60,10 @@ if (!$success) {
     die("Couldn't move uploaded files");
 }
 if (isset ($_FILES["psd"]) && ($_FILES['psd']['error'] == UPLOAD_ERR_OK)){
+	if(!SIZE_CHECK || ($_FILES['picture']['size'] < (PSD_MAX_KB * 1024))){
 	//PSDファイルのアップロードができなかった場合はエラーメッセージはださず、画像のみ投稿する。 
 	move_uploaded_file($_FILES['psd']['tmp_name'], TEMP_DIR.$imgfile.'.psd');
+	}
 }
 
 $u_ip = getenv("HTTP_CLIENT_IP");
