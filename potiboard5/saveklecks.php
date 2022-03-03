037762fcec76a795b11ee48848a34d4faee982a3
satopian <satopian@mia.eek.jp>
2022-03-04 01:16:54 +0900

    v5.07.5

--- potiboard5/saveklecks.php
+++ potiboard5/saveklecks.php
@@ -38,13 +38,6 @@ if(mime_content_type($_FILES['picture']['tmp_name'])!=='image/png'){
 	die("Your picture upload failed! Please try again!");
 }
 
-list($w,$h)=getimagesize($_FILES['picture']['tmp_name']);
-
-if($w > PMAX_W || $h > PMAX_H){//幅と高さ
-	//規定サイズ違反を検出しました。画像は保存されません。
-	die("Your picture upload failed! Please try again!");
-}
-
 $success = TRUE;
 $success = $success && move_uploaded_file($_FILES['picture']['tmp_name'], TEMP_DIR.$imgfile.'.png');
 
