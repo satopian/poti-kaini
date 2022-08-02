2d9baf53a48411de505e096badc83403f4a38fe9
satopian <satopian@mia.eek.jp>
2022-08-02 14:39:42 +0900

    neoを使用する場合は、CheerpJのJavaScript を挿入しない。

--- potiboard5/templates/mono/mono_paint.blade.php
+++ potiboard5/templates/mono/mono_paint.blade.php
@@ -100,8 +100,9 @@
 <script src="chickenpaint/js/chickenpaint.min.js?{{$parameter_day}}"></script>
 <link rel="stylesheet" type="text/css" href="chickenpaint/css/chickenpaint.css?{{$parameter_day}}">
 
-		@else 
-		{{-- <!-- Javaが使えるかどうか判定 使えなければcheerpJをロード --> --}}
+	@else 
+		@if(($paint_mode and !$useneo) or ($pch_mode and !$type_neo))
+		{{--  Javaが使えるかどうか判定 使えなければcheerpJをロード  --}}
 		<script>
 			function cheerpJLoad() {
 			var jEnabled = navigator.javaEnabled();
@@ -115,7 +116,7 @@
 			}
 			window.addEventListener("load", function() { cheerpJLoad(); }, false);
 		</script>
-
+		@endif
 	@endif
 	@if($paint_mode)
 	<style>body{overscroll-behavior-x: none !important; }</style>
