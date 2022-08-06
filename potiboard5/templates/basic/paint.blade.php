8aa5512f9f04aee5a97bbf251c1b3169ffb930c1
satopian <satopian@mia.eek.jp>
2022-08-06 14:14:26 +0900

    描画時間。setTimeout の引数に文字列で関数を指定していた箇所を修正。CSPでevalの違反になっていたのを修正。

--- potiboard5/templates/basic/paint.blade.php
+++ potiboard5/templates/basic/paint.blade.php
@@ -405,7 +405,7 @@ if(DynamicColor) PaletteListSetColor();
 <INPUT SIZE="20" NAME="count">
 </FORM>
 <Script>
-	timerID=10;stime=new Date;function SetTimeCount(){now=new Date;s=Math.floor((now.getTime()-stime.getTime())/1E3);disp="";86400<=s&&(d=Math.floor(s/86400),disp+=d+"\u65e5",s-=86400*d);3600<=s&&(h=Math.floor(s/3600),disp+=h+"\u6642\u9593",s-=3600*h);60<=s&&(m=Math.floor(s/60),disp+=m+"\u5206",s-=60*m);document.watch.count.value=disp+s+"\u79d2";clearTimeout(timerID);timerID=setTimeout("SetTimeCount()",250)}SetTimeCount();
+	timerID=10;stime=new Date;function SetTimeCount(){now=new Date;s=Math.floor((now.getTime()-stime.getTime())/1E3);disp="";86400<=s&&(d=Math.floor(s/86400),disp+=d+"\u65e5",s-=86400*d);3600<=s&&(h=Math.floor(s/3600),disp+=h+"\u6642\u9593",s-=3600*h);60<=s&&(m=Math.floor(s/60),disp+=m+"\u5206",s-=60*m);document.watch.count.value=disp+s+"\u79d2";clearTimeout(timerID);timerID=setTimeout(function() { SetTimeCount(); }, 250);}SetTimeCount();
 	</Script>
 	</div>
 <!--描画時間動的表示 ここまで-->
