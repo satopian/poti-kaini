95f26ea12ccd1ca6626ecde412216e0eec5d2a14
satopian <satopian@mia.eek.jp>
2022-08-16 14:57:29 +0900

    MONOの時計の表示を日本語に。○時間○分○秒。

--- potiboard5/templates/mono/mono_paint.blade.php
+++ potiboard5/templates/mono/mono_paint.blade.php
@@ -397,35 +397,10 @@
 								PaintTime :
 								<input type="text" size="24" name="count">
 							</p>
-							<script>
-								timerID = 10;
-								stime = new Date();
-								function SetTimeCount() {
-									now = new Date();
-									s = Math.floor((now.getTime() - stime.getTime())/1000);
-									disp = '';
-									if(s >= 86400){
-										d = Math.floor(s/86400);
-										disp += d+"day ";
-										s -= d*86400;
-									}
-									if(s >= 3600){
-										h = Math.floor(s/3600);
-										disp += h+"hr ";
-										s -= h*3600;
-									}
-									if(s >= 60){
-										m = Math.floor(s/60);
-										disp += m+"min ";
-										s -= m*60;
-									}
-									document.watch.count.value = disp+s+"sec";
-									clearTimeout(timerID);
-									timerID = setTimeout(function() { SetTimeCount(); },250);
-								}
-								SetTimeCount();
-							</script>
-						</form>
+							<Script>
+								timerID = 10; stime = new Date; function SetTimeCount() { now = new Date; s = Math.floor((now.getTime() - stime.getTime()) / 1E3); disp = ""; 86400 <= s && (d = Math.floor(s / 86400), disp += d + "\u65e5", s -= 86400 * d); 3600 <= s && (h = Math.floor(s / 3600), disp += h + "\u6642\u9593", s -= 3600 * h); 60 <= s && (m = Math.floor(s / 60), disp += m + "\u5206", s -= 60 * m); document.watch.count.value = disp + s + "\u79d2"; clearTimeout(timerID); timerID = setTimeout(function () { SetTimeCount(); }, 250); } SetTimeCount();
+							</Script>
+													</form>
 					<hr>
 					</div>
 				</div>
