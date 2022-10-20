43bf0c226b6c74118d496cc9d9e1d1a669c43dab
satopian <satopian@mia.eek.jp>
2022-10-20 22:54:52 +0900

    Tweet(頭文字は大文字ルール

--- potiboard5/templates/basic/res.blade.php
+++ potiboard5/templates/basic/res.blade.php
@@ -160,10 +160,10 @@
 					<span class="share_button">
 						<a target="_blank"
 							href="https://twitter.com/intent/tweet?text=%5B{{$ress[0]['encoded_no']}}%5D%20{{$ress[0]['share_sub']}}%20by%20{{$ress[0]['share_name']}}%20-%20{{$encoded_title}}&url={{$encoded_rooturl}}{{$encoded_self}}?res={{$ress[0]['encoded_no']}}"><span
-								class="icon-twitter"></span>tweet</a>
+								class="icon-twitter"></span>Tweet</a>
 						<a target="_blank" class="fb btn"
 							href="http://www.facebook.com/share.php?u={{$encoded_rooturl}}{{$encoded_self}}?res={{$ress[0]['encoded_no']}}"><span
-								class="icon-facebook2"></span>share</a>
+								class="icon-facebook2"></span>Share</a>
 					</span>
 					@endif
 
