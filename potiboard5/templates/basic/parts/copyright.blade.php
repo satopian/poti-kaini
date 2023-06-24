7989c99be806ed29706da55d1cb47f9537e69a9b
satopian <satopian@mia.eek.jp>
2023-06-24 21:32:35 +0900

    Tegaki、著作リンク。動画保存しないときは録画しない。

--- potiboard5/templates/basic/parts/copyright.blade.php
+++ potiboard5/templates/basic/parts/copyright.blade.php
@@ -7,7 +7,7 @@
 		OriginalScript - <a href="http://www.punyu.net/" target="_blank" rel="noopener noreferrer" title="POTI-board v1.32 (by ぷにゅねっと)">POTI-board</a>
 	<span title="futaba.php v0.8 lot.031015 (by ふたば)">futaba.php</span> <a href="http://php.loglog.jp/" target="_blank" rel="noopener noreferrer" title="gazou.php v3.0 (by レッツPHP!)">gazou.php</a><br>
 	@if($paint) 
-	OekakiApp - <a href="http://hp.vector.co.jp/authors/VA016309/" target="_blank" rel="noopener noreferrer" title="by しぃちゃん">Shi-Painter</a> , <a href="http://github.com/funige/neo/" target="_blank" rel="noopener noreferrer" title="by funige">PaintBBS NEO</a>  , <a href="https://bitbof.com/"  target="_blank" rel="noopener noreferrer" title="by bitbof">klecks</a> , <a href="https://github.com/thenickdude/chickenpaint" target="_blank" rel="noopener noreferrer" title="by Nicholas Sherlock">ChickenPaint</a>
+	OekakiApp - <a href="http://hp.vector.co.jp/authors/VA016309/" target="_blank" rel="noopener noreferrer" title="by しぃちゃん">Shi-Painter</a> , <a href="http://github.com/funige/neo/" target="_blank" rel="noopener noreferrer" title="by funige">PaintBBS NEO</a>  , <a href="https://github.com/desuwa/tegaki" target="_blank" rel="noopener noreferrer" title="by Maxime Youdine">Tegaki</a> , <a href="https://bitbof.com/"  target="_blank" rel="noopener noreferrer" title="by bitbof">klecks</a> , <a href="https://github.com/thenickdude/chickenpaint" target="_blank" rel="noopener noreferrer" title="by Nicholas Sherlock">ChickenPaint</a>
 	<br>
 	 @endif
 	UseFunction - <span title="by WonderCatStudio">動的パレットスクリプト</span>
