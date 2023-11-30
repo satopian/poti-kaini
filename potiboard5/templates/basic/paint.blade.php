46222a516777a618bb4385405840091ba24c11c1
satopian <satopian@mia.eek.jp>
2023-11-30 17:48:38 +0900

    Firefoxのメニューが開閉するためNEO、しぃペインターを起動時の、altキーのキーアップのデフォルトの動作をキャンセル。

--- potiboard5/templates/basic/paint.blade.php
+++ potiboard5/templates/basic/paint.blade.php
@@ -116,6 +116,19 @@
 	window.addEventListener('DOMContentLoaded',neo_add_disable_touch_move,false);
 </script>
 @endif
+@if($paint_mode) 
+@if(!$chickenpaint)
+<script>
+	//Firefoxのメニューバーが開閉するのため、Altキーのデフォルトの処理をキャンセル
+	document.addEventListener('keyup', function(e) {//しぃペインター NEO共通
+		// e.key を利用して特定のキーのアップイベントを検知する
+		if (e.key.toLowerCase() === 'alt') {
+			e.preventDefault(); // Alt キーのデフォルトの動作をキャンセル
+		}
+	});
+</script>
+@endif
+@endif
 @if($pch_mode and $type_neo) 
 <link rel="stylesheet" href="neo.css?{{$parameter_day}}&{{$ver}}">
 <script src="neo.js?{{$parameter_day}}&{{$ver}}"></script>
