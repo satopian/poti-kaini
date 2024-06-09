731b9ab81bd4c57cbb237c395ac6bbf35164de6a
satopian <satopian@mia.eek.jp>
2024-06-09 23:21:13 +0900

    NEOの下部の時計をタップしてもスクリーンキーボードが出現しないように調整。

--- potiboard5/templates/basic/paint.blade.php
+++ potiboard5/templates/basic/paint.blade.php
@@ -542,9 +542,9 @@ if(DynamicColor) PaletteListSetColor();
 <div class="centering">
 <!--描画時間動的表示-->
 <div class="applet_painttime">
-<FORM NAME="watch">描画時間
-<INPUT SIZE="20" NAME="count">
-</FORM>
+<form name="watch">描画時間 
+	<input type="text" size="20" name="count" class="input_count_timer" readonly>
+</form>
 <Script>
 	timerID=10;stime=new Date;function SetTimeCount(){now=new Date;s=Math.floor((now.getTime()-stime.getTime())/1E3);disp="";86400<=s&&(d=Math.floor(s/86400),disp+=d+"\u65e5",s-=86400*d);3600<=s&&(h=Math.floor(s/3600),disp+=h+"\u6642\u9593",s-=3600*h);60<=s&&(m=Math.floor(s/60),disp+=m+"\u5206",s-=60*m);document.watch.count.value=disp+s+"\u79d2";clearTimeout(timerID);timerID=setTimeout(function() { SetTimeCount(); },250);};
 	document.addEventListener('DOMContentLoaded',SetTimeCount,false);
