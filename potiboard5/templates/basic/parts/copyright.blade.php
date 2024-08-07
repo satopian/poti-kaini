ebad26edf1b67374dbf61e4d5bd7d7ef10b1269e
satopian <satopian@mia.eek.jp>
2024-08-07 20:59:06 +0900

    AXNOS Paint対応テンプレート。

--- potiboard5/templates/basic/parts/copyright.blade.php
+++ potiboard5/templates/basic/parts/copyright.blade.php
@@ -7,7 +7,9 @@
 		OriginalScript - <a href="http://www.punyu.net/" target="_blank" rel="noopener noreferrer" title="POTI-board v1.32 (by ぷにゅねっと)">POTI-board</a>
 	<span title="futaba.php v0.8 lot.031015 (by ふたば)">futaba.php</span> <a href="http://php.loglog.jp/" target="_blank" rel="noopener noreferrer" title="gazou.php v3.0 (by レッツPHP!)">gazou.php</a><br>
 	@if($paint) 
-	OekakiApp - <a href="http://hp.vector.co.jp/authors/VA016309/" target="_blank" rel="noopener noreferrer" title="by しぃちゃん">Shi-Painter</a> , <a href="http://github.com/funige/neo/" target="_blank" rel="noopener noreferrer" title="by funige">PaintBBS NEO</a>  , <a href="https://github.com/desuwa/tegaki" target="_blank" rel="noopener noreferrer" title="by Maxime Youdine">Tegaki</a> , <a href="https://github.com/bitbof/klecks"  target="_blank" rel="noopener noreferrer" title="by bitbof">Klecks</a> , <a href="https://github.com/satopian/ChickenPaint_Be" target="_blank" rel="noopener noreferrer" title="by satopian">ChickenPaint Be</a>
+	OekakiApp - <a href="http://hp.vector.co.jp/authors/VA016309/" target="_blank" rel="noopener noreferrer" title="by しぃちゃん">Shi-Painter</a> , <a href="http://github.com/funige/neo/" target="_blank" rel="noopener noreferrer" title="by funige">PaintBBS NEO</a>  , <a href="https://github.com/desuwa/tegaki" target="_blank" rel="noopener noreferrer" title="by Maxime Youdine">Tegaki</a> , <a href="https://github.com/bitbof/klecks"  target="_blank" rel="noopener noreferrer" title="by bitbof">Klecks</a> 
+	, <a href="https://github.com/axnospaint/axnospaint-lib" target="_blank" rel="noopener noreferrer" title="by Axnos Paint">Axnos Paint</a>
+	, <a href="https://github.com/satopian/ChickenPaint_Be" target="_blank" rel="noopener noreferrer" title="by satopian">ChickenPaint Be</a>
 	<br>
 	 @endif
 	UseFunction - <span title="by WonderCatStudio">動的パレットスクリプト</span>
