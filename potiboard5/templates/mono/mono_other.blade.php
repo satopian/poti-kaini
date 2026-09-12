e7857a7a4067ba295b03121e1d0c8838492136dd
satopian <satopian@mia.eek.jp>
2026-09-12 20:08:07 +0900

    画像プレビューのhtmlの文法エラーを修正。(動作変わらず)

--- potiboard5/templates/mono/mono_other.blade.php
+++ potiboard5/templates/mono/mono_other.blade.php
@@ -173,7 +173,7 @@
 								<br>
 								<small>（添付可能なファイル形式：GIF、JPG、PNG、WEBP）</small><br>
 								{{-- プレビュー表示 --}}
-							<img id="attach_preview" style="max-width:200px;max-height:200px; display:none;">
+							<img id="attach_preview" style="max-width:200px;max-height:200px; display:none;" alt="preview" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==">
 							</td>
 						</tr>
 						@endif
